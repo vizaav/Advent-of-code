f9929d299cc9c82b6660f168bb533f92e78d671c
Iza <58888512+vizaav@users.noreply.github.com>
2022-12-07 03:11:08 +0100

    zad6.1 i  zad6.2

--- adwent06.1/src/com/company/Main.java
+++ adwent06.1/src/com/company/Main.java
@@ -8,24 +8,51 @@ import java.util.stream.Stream;
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
-	File file = new File("src/com/company/input.txt");
-    Scanner scanner = new Scanner(file);
-    String line = scanner.nextLine();
+        File file = new File("src/com/company/input.txt");
+        Scanner scanner = new Scanner(file);
+        String line = scanner.nextLine();
         System.out.println("Liczba znaków: " + fourDifferent(line));
+        System.out.println("Liczba znaków(14): " + fourteenDifferent(line));
     }
 
-    public static int fourDifferent(String e){
+    public static int fourDifferent(String e) {
         char[] chars = e.toCharArray();
-       int counter = 4;
+        int counter = 4;
         for (int i = 0; i < chars.length; i++) {
-            if(chars[i] != chars[i+1] && chars[i] != chars[i+2] && chars[i] != chars[i+3] && chars[i+1] != chars[i+2] && chars[i+1] != chars[i+3] && chars[i+2] != chars[i+3]){
+            if (chars[i] != chars[i + 1] && chars[i] != chars[i + 2] && chars[i] != chars[i + 3] && chars[i + 1] != chars[i + 2] && chars[i + 1] != chars[i + 3] && chars[i + 2] != chars[i + 3]) {
                 return counter;
-            }else{
+            } else {
                 counter++;
             }
 
-            }
-        return -1;
         }
+        return -1;
     }
+
+    public static int fourteenDifferent(String e) {
+        int counter = 14;
+        for (int i = 0; i < e.length() - 14; i++) {
+            if (e.substring(i, i + 14).chars().distinct().count() == 14) {
+                return counter;
+            } else {
+                counter++;
+            }
+        }
+        return -1;
+    }
+
+
+
+    public static boolean uniqueCharacters(String e) {
+        char[] chars = e.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            for (int j = 0; j < chars.length; j++) {
+                if (chars[i] == chars[j] && i != j) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
 
